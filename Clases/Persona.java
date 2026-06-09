a3ab3ed93d2975bad9a3145c3fe8e3de5ec02eba
lautyth <lautarochacon99@gmail.com>
2026-06-09 20:11:09 -0300

    Creación del repositorio

---
+++ Clases/Persona.java
@@ -0,0 +1,18 @@
+import java.sql.Date;
+
+public abstract class persona {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String correo;
+    private String contraseña;
+    private String legajo;
+    public Date fecha_registro;
+
+    
+    
+    public persona(){
+        
+
+    }
+}
